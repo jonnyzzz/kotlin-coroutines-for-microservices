3e8026608161cdf713552f2ed1ea621a4ff01613
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-10-11 09:08:22 +0200

    example with threads

--- service-threads/src/main/kotlin/main.kt
+++ service-threads/src/main/kotlin/main.kt
@@ -1,5 +1,16 @@
 package org.jonnyzzz.threads
 
+import java.util.concurrent.CyclicBarrier
+
 fun main(args: Array<String>) {
   println("Threads example")
+
+  val threads = 10_000
+  val sem = CyclicBarrier(threads + 1)
+  List(threads) {
+    Thread(Runnable { sem.await() }).apply { start() }
+  }
+
+  sem.await()
+  println("All threads are there!")
 }

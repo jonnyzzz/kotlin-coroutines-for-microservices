16ced9742f63f28e07ccc7e27b95bc29b5381499
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-10-17 13:03:52 +0200

    more examples

--- service-threads/src/main/kotlin/co-main3.kt
+++ service-threads/src/main/kotlin/co-main3.kt
@@ -6,15 +6,20 @@ import kotlinx.coroutines.launch
 suspend fun main() = coroutineScope {
   println("Coroutines example")
 
+coroutineScope {
   val threads = 1_000_000
   val barrier = MutexBarrier(threads + 1)
   repeat(threads) {
-    launch { //starts a coroutine
+    launch {
+      //starts a coroutine
       barrier.await() //non-blocking suspend
     }
   }
 
   barrier.await()
+}
+
+
   println("All threads are there!")
 }
 

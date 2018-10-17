da189a4481f5dd50a136cefd81e20d5bfe8252c8
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-10-17 23:14:32 +0200

    more examples

---
+++ service-threads/src/main/kotlin/sequence.kt
@@ -0,0 +1,22 @@
+package org.jonnyzzz.threads
+
+
+fun main(args: Array<String>) {
+
+//fibonacci
+  val seq = sequence {
+    var f0 = 0
+    var f1 = 1
+    while (true) {
+      val fN = f0 + f1
+      f0 = f1
+      f1 = fN
+
+      println("yield - $fN")
+      yield(fN)
+    }
+  }.take(10).forEach {
+    println("got   - $it")
+  }
+
+}

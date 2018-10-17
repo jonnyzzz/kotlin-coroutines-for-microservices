4ca088ee735633cc47199e6290e18db4153bb119
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-10-17 12:52:56 +0200

    simplify example

--- service-threads/src/main/kotlin/simple-main.kt
+++ service-threads/src/main/kotlin/simple-main.kt
@@ -1,18 +1,15 @@
 package org.jonnyzzz.threads
 
 import java.util.concurrent.CyclicBarrier
-import java.util.concurrent.Semaphore
 
 fun main() {
   println("Threads example")
 
   val threads = 1_000_000
   val barrier = CyclicBarrier(threads + 1)
-  val sem = Semaphore(0)
   repeat(threads) {
-    Thread{
+    Thread {
       barrier.await()
-      sem.acquire()
     }.start()
   }
 

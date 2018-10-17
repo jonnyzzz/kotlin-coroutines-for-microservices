d6135034c730b88d928cc98ae140542921a21ebb
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-10-17 17:55:18 +0200

    more examples

---
+++ service-threads/src/main/kotlin/okhttp.kt
@@ -0,0 +1,48 @@
+package org.jonnyzzz.threads
+
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+  suspend fun suspendingCall() =
+    suspendCoroutine<String> { continuation ->
+      //do what every you like
+
+      //report result
+      continuation.resume("result")
+      //OR
+      //make it fail
+      continuation.resumeWithException(RuntimeException())
+    }
+
+
+
+suspend fun okSuspendingCall(client: OkHttpClient, request: Request): String {
+
+  return suspendCoroutine { continuation ->
+    client.newCall(request)
+            .enqueue(object : Callback {
+
+      override fun onFailure(call: Call, e: IOException) {
+        continuation.resumeWithException(e)
+      }
+
+      override fun onResponse(call: Call, response: Response) {
+        continuation.resume(response.body()?.string() ?: "")
+      }
+    })
+  }
+
+}
+
+
+suspend fun main(args: Array<String>) {
+  ::suspendingCall.name
+  ::okSuspendingCall.name
+}
